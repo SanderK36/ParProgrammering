f7d581471c47a8d536a05f9114c0eeac57b52b9d
SanderK36 <sander.k606@gmail.com>
2025-06-02 13:47:46 +0200

    02.06

---
+++ Chatbot/Chatbot/Chatbot.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Chatbot
+{
+    internal class Chatbot
+    {
+        public string Navn { get; set; }
+        public List<string> Svar { get; set; }
+
+        public Chatbot(string navn)
+        {
+            Navn = navn;
+            Svar = new List<string>();
+        }
+
+        public string TilfeldigSvar()
+        {
+            
+            Random random = new Random();
+            int index = random.Next(Svar.Count);
+            return Svar[index];
+        }
+    }
+    
+}
---
+++ Chatbot/Chatbot/Program.cs
@@ -0,0 +1,67 @@
+﻿namespace Chatbot
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Chatbot bot = null;
+            while (true)
+            {
+                Console.WriteLine("Chatbot Lager");
+                Console.WriteLine("1. Lag en ny chatbot");
+                Console.WriteLine("2. Snakk med chatbot");
+                Console.WriteLine("3. Avslutt");
+                Console.Write("Velg et alternativ: ");
+                string valg = Console.ReadLine();
+
+
+                switch (valg)
+                {
+                    case "1":
+                        Console.WriteLine("Gi botten et navn");
+                        string navn = Console.ReadLine();
+                        bot = new Chatbot(navn);
+
+                        Console.WriteLine("skriv inn svar");
+                        while (true)
+                        {
+                            Console.WriteLine("svar:");
+                            string svar = Console.ReadLine();
+                            if (svar.ToLower() == "ferdig") break;
+                            if(!string.IsNullOrWhiteSpace(svar))
+                                bot.Svar.Add(svar);
+                        }
+                        break;
+
+                    case "2":
+                        if (bot == null)
+                        {
+                            Console.WriteLine("Ingen chatbot å prate med");
+                        }
+                        else
+                        {
+                            Console.WriteLine($"du prater med {bot.Navn}");
+                            while (true)
+                            {
+                                Console.Write("Du:");
+                                string melding = Console.ReadLine();
+                                if(melding.ToLower() == "ferdig") break;
+                                Console.WriteLine($"{bot.Navn}: {bot.TilfeldigSvar()} ");
+                            }
+                        }
+                        break;
+
+
+                    case "3":
+                        Console.WriteLine("Avslutter programmet...");
+                        return;
+
+                    default:
+                        Console.WriteLine("Ugyldig valg, prøv igjen.");
+                        break;
+                }
+            } 
+        } 
+    }
+}
